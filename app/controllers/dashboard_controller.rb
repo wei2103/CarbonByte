8ad02c01fe57f45b698bc46df2a0bed01e26fd4d
Max Yeap <max@yongtuck.com>
2024-03-12 12:51:11 +0800

    Update to grab the data of the user instead of everyone's data

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -4,8 +4,8 @@ class DashboardController < ApplicationController
   def index
     @activities = Activity.all
 
-    @carbon_emissions_by_activity_type = Activity.group("activity_types.name").joins(:activity_type).sum(:carbon_emission)
+    @carbon_emissions_by_activity_type = Activity.where(user_id: current_user.id).group("activity_types.name").joins(:activity_type).sum(:carbon_emission)
 
-    @carbon_emissions_by_category = Activity.group("activity_types.category").joins(:activity_type).sum(:carbon_emission)
+    @carbon_emissions_by_category = Activity.where(user_id: current_user.id).group("activity_types.category").joins(:activity_type).sum(:carbon_emission)
   end
 end
