7f561a4acf4dcdc5b2a40be28878cdc9480126c0
Max Yeap <max@yongtuck.com>
2024-03-13 07:41:44 +0800

    Sample layout

--- app/controllers/activity_types_controller.rb
+++
@@ -1,6 +0,0 @@
-class ActivityTypesController < ApplicationController
-  def show
-    activity_type = ActivityType.find(params[:id])
-    render json: activity_type
-  end
-end
--- spec/factories/activity_type.rb
+++
@@ -1,6 +0,0 @@
-FactoryBot.define do
-  factory :activity_type do
-    sequence(:name) { |n| "activity_type_#{n}" } # Generates unique name for each factory
-    # Add any other attributes you need for your activity type
-  end
-end
