c232c2e8a71103538010489873874e8cf953315d
Max Yeap <max@yongtuck.com>
2024-04-08 01:47:29 +0800

    Remove association between reply and user

--- app/models/reply.rb
+++ app/models/reply.rb
@@ -1,5 +1,5 @@
 class Reply < ApplicationRecord
-  belongs_to :user
+  # belongs_to :user
   belongs_to :post
 
   # If the text limit over 280 characters, it will truncate the message and add ... at the end
