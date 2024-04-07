645488c355d7ef613597610481cfd69b853fa04e
Max Yeap <max@yongtuck.com>
2024-04-08 05:33:16 +0800

    Add the user_id back to replies table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_04_07_174447) do
+ActiveRecord::Schema[7.0].define(version: 2024_04_06_141033) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -96,6 +96,7 @@ ActiveRecord::Schema[7.0].define(version: 2024_04_07_174447) do
     t.string "message"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "user_id"
     t.index ["post_id"], name: "index_replies_on_post_id"
   end
 
@@ -128,4 +129,5 @@ ActiveRecord::Schema[7.0].define(version: 2024_04_07_174447) do
   add_foreign_key "goals", "users"
   add_foreign_key "posts", "users"
   add_foreign_key "replies", "posts"
+  add_foreign_key "replies", "users"
 end
