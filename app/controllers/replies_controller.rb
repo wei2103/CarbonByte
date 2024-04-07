76620d5ff2e3051dde469df7603a9797a48a71c3
Max Yeap <max@yongtuck.com>
2024-04-08 01:52:01 +0800

    Create create method

--- app/controllers/replies_controller.rb
+++ app/controllers/replies_controller.rb
@@ -1,13 +1,18 @@
 class RepliesController < ApplicationController
   def create
     @post = Post.find(params[:post_id])
-    @reply = @post.replies.create(reply_params)
-    redirect_to post_path(@post)
+    puts params["message"]
+    @reply = @post.replies.new(message: params[:reply][:message])
+    if @reply.save!
+      redirect_to @post, notice: 'Reply was successfully created.'
+    else
+      render 'posts/show'
+    end
   end
 
   private
 
   def reply_params
-    params.require(:reply).permit(:name, :reply)
+    params.require(:reply).permit(:message)
   end
 end
