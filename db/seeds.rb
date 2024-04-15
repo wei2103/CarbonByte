f105658892dfac6a065566637cef36451ce28398
MengFoong <mengfoong2003@gmail.com>
2024-04-15 23:33:52 +0800

    delete cache file

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,6 +8,9 @@
 # db/seeds/activity_types.rb
 
 # Seed data for activity types
+# admin_user = User.create(email: 'admin@example.com', password: 'password', admin: true)
+user = User.create(email: 'user@example.com', password: 'password')
+####################################################################################################################
 # activity_types_data = [
 #   {
 #     name: "Motorcycle",
@@ -59,65 +62,45 @@
 #     category: "energy"
 #   }
 # ]
-
+#
 # # Create ActivityType records
 # activity_types_data.each do |activity_type_data|
 #   ActivityType.create!(activity_type_data)
 # end
+##################################################################################################
+#
+Content.create(
+  user: user,
+  content_type: 'video',
+  content_link: 'https://www.youtube.com/embed/J_iDcKDAwbA',
+  title: 'How to Reduce Your Carbon Footprint',
+  description: 'Video is about how to Reduce Your Carbon Footprint. A carbon footprint is the total amount of greenhouse gases, including carbon dioxide,
+  that are emitted into the atmosphere as a result of our activities.'
+)
 
-#admin_user = User.create(email: 'admin@example.com', password: 'password', admin: true)
-user = User.create(email: 'user@example.com', password: 'password')
+Content.create(
+  user: user,
+  content_type: 'article',
+  content_link: 'https://www.fssi-ca.com/wp-content/uploads/MvF_footprint.png',
+  title: 'What is a carbon footprint?',
+  description: 'So, what exactly is a carbon footprint? According to Mike Berners-Lee, a professor at Lancaster University in the UK and author of The Carbon Footprint of Everything, it is “the sum total of all the greenhouse gas emissions that had to take place in order for a product to be produced or for an activity to take place.”
+  For most consumers in developed countries, these products and activities tend to fall into four principal categories: household energy use, transport, food, and everything else, which is mostly the products we buy, from utensils to clothes to cars to television sets.
+  Each of these activities and products has its own footprint; a person’s carbon footprint is the combined total of the products they buy and use, the activities they undertake, and so on. A person who regularly consumes beef will have a  larger food footprint than his vegan neighbor, but that neighbor’s overall footprint may be larger if she drives an hour to work and back in an SUV each day while our meat-eater bicycles to his office nearby. Both their footprints may pale in comparison to the businesswoman across the street, who flies first-class cross-country twice a month.
+  Unsurprisingly, in general terms the size of a person’s carbon footprint tends to increase with wealth. In his book, Berners-Lee writes that the average global citizen has a carbon footprint that is equivalent to the emission of seven tons of carbon dioxide per year. However, that figure is approximately 13 tons for the average Briton and roughly 21 tons per person in the United States.; The “average American takes just a couple of days to match the annual footprint of the average Nigerian or Malian,” he writes.'
+)
 
-# Content.create(
-#   user: user,
-#   content_type: 'video',
-#   content_link: 'https://www.youtube.com/embed/J_iDcKDAwbA',
-#   title: 'How to Reduce Your Carbon Footprint',
-#   description: 'Video is about how to Reduce Your Carbon Footprint. A carbon footprint is the total amount of greenhouse gases, including carbon dioxide,
-#   that are emitted into the atmosphere as a result of our activities.'
-# )
+Content.create(
+  user: user,
+  content_type: 'infographic',
+  content_link: 'https://www.epa.gov/sites/default/files/2017-04/fossil_fuels_1.png',
+  title: 'Trends in Global Emissions',
+  description: 'Global carbon emissions from fossil fuels have significantly increased since 1900. Since 1970, CO2 emissions have increased by about 90%, with emissions from fossil fuel combustion and industrial processes contributing about 78% of the total greenhouse gas emissions increase from 1970 to 2011. Agriculture, deforestation, and other land-use changes have been the second-largest contributors.[1]
+  Emissions of non-CO2 greenhouse gases have also increased significantly since 1900. To learn more about past and projected global emissions of non-CO2 gases, please see the EPA report, Global Anthropogenic Non-CO2 Greenhouse Gas Emissions: 1990-2020.'
+)
 
-# Content.create(
-#   user: user,
-#   content_type: 'article',
-#   content_link: 'https://www.fssi-ca.com/wp-content/uploads/MvF_footprint.png',
-#   title: 'What is a carbon footprint?',
-#   description: 'So, what exactly is a carbon footprint? According to Mike Berners-Lee, a professor at Lancaster University in the UK and author of The Carbon Footprint of Everything, it is “the sum total of all the greenhouse gas emissions that had to take place in order for a product to be produced or for an activity to take place.”
-#   For most consumers in developed countries, these products and activities tend to fall into four principal categories: household energy use, transport, food, and everything else, which is mostly the products we buy, from utensils to clothes to cars to television sets.
-#   Each of these activities and products has its own footprint; a person’s carbon footprint is the combined total of the products they buy and use, the activities they undertake, and so on. A person who regularly consumes beef will have a  larger food footprint than his vegan neighbor, but that neighbor’s overall footprint may be larger if she drives an hour to work and back in an SUV each day while our meat-eater bicycles to his office nearby. Both their footprints may pale in comparison to the businesswoman across the street, who flies first-class cross-country twice a month.
-#   Unsurprisingly, in general terms the size of a person’s carbon footprint tends to increase with wealth. In his book, Berners-Lee writes that the average global citizen has a carbon footprint that is equivalent to the emission of seven tons of carbon dioxide per year. However, that figure is approximately 13 tons for the average Briton and roughly 21 tons per person in the United States.; The “average American takes just a couple of days to match the annual footprint of the average Nigerian or Malian,” he writes.'
-# )
-
-# Content.create(
-#   user: user,
-#   content_type: 'infographic',
-#   content_link: 'https://www.epa.gov/sites/default/files/2017-04/fossil_fuels_1.png',
-#   title: 'Trends in Global Emissions',
-#   description: 'Global carbon emissions from fossil fuels have significantly increased since 1900. Since 1970, CO2 emissions have increased by about 90%, with emissions from fossil fuel combustion and industrial processes contributing about 78% of the total greenhouse gas emissions increase from 1970 to 2011. Agriculture, deforestation, and other land-use changes have been the second-largest contributors.[1]
-#   Emissions of non-CO2 greenhouse gases have also increased significantly since 1900. To learn more about past and projected global emissions of non-CO2 gases, please see the EPA report, Global Anthropogenic Non-CO2 Greenhouse Gas Emissions: 1990-2020.'
-# )
-
-# activity_type_1 = ActivityType.create(name: 'Motorcycle', category: 'Transport', climatiq_unit_of_measurement: 'km')
-# activity_type_2 = ActivityType.create(name: 'Car', category: 'Transport', climatiq_unit_of_measurement: 'km')
-# activity_type_3 = ActivityType.create(name: 'Fresh Fruit', category: 'Food', climatiq_unit_of_measurement: 't')
-# activity_type_4 = ActivityType.create(name: 'Car', category: 'Transport', climatiq_unit_of_measurement: 'km')
-# activity_type_5 = ActivityType.create(name: 'Fresh Fruit', category: 'Food', climatiq_unit_of_measurement: 't')
-# activity_type_6 = ActivityType.create(name: 'Car', category: 'Transport', climatiq_unit_of_measurement: 'km')
-# activity_type_7 = ActivityType.create(name: 'Fresh Fruit', category: 'Food', climatiq_unit_of_measurement: 't')
-
-# Create mock Activities associated with ActivityTypes
-# Activity.create([
-#   { user_id: 4, carbon_emission: 134, type_id: 1, value: 10, created_at: Time.now - 1.day, updated_at: Time.now - 1.day },
-#   { user_id: 4, carbon_emission: 15, type_id: 3, value: 34, created_at: Time.now - 2.days, updated_at: Time.now - 2.days },
-#   { user_id: 4, carbon_emission: 50, type_id: 4, value: 5, created_at: Time.now - 3.days, updated_at: Time.now - 3.days },
-#   { user_id: 4, carbon_emission: 150, type_id: 3, value: 188 created_at: Time.now - 4.days, updated_at: Time.now - 4.days },
-#   { user_id: 4, carbon_emission: 44, type_id: 4, value: 4, created_at: Time.now - 5.days, updated_at: Time.now - 5.days },
-#   { user_id: 4, carbon_emission: 66, type_id: 3, value: 16, created_at: Time.now - 6.days, updated_at: Time.now - 5.days },
-#   { user_id: 4, arbon_emission: 22, type_id: 4, value: 1, created_at: Time.now - 7.days, updated_at: Time.now - 5.days  },
-# ])
 
 Activity.create(
-  user_id: 4,
+  user_id: 1,
   carbon_emission: 134,
   type_id: 1,
   value: 10,
@@ -126,7 +109,7 @@ Activity.create(
 )
 
 Activity.create(
-  user_id: 4,
+  user_id: 1,
   carbon_emission: 15,
   type_id: 3,
   value: 34,
@@ -135,7 +118,7 @@ Activity.create(
 )
 
 Activity.create(
-  user_id: 4,
+  user_id: 1,
   carbon_emission: 50,
   type_id: 4,
   value: 5,
@@ -144,7 +127,7 @@ Activity.create(
 )
 
 Activity.create(
-  user_id: 4,
+  user_id: 1,
   carbon_emission: 150,
   type_id: 3,
   value: 188,
@@ -153,7 +136,7 @@ Activity.create(
 )
 
 Activity.create(
-  user_id: 4,
+  user_id: 1,
   carbon_emission: 44,
   type_id: 4,
   value: 4,
@@ -162,7 +145,7 @@ Activity.create(
 )
 
 Activity.create(
-  user_id: 4,
+  user_id: 1,
   carbon_emission: 66,
   type_id: 3,
   value: 16,
@@ -171,7 +154,7 @@ Activity.create(
 )
 
 Activity.create(
-  user_id: 4,
+  user_id: 1,
   carbon_emission: 22,
   type_id: 4,
   value: 1,
